95e778872e9a3763331122f875cb63104b27b5c6
dyannobt <30714523+dyannobt@users.noreply.github.com>
2017-10-07 12:59:24 +1100

    updated colours

--- archive/assets/js/main.js
+++ archive/assets/js/main.js
@@ -28,10 +28,67 @@ $('.nav_blue').click(function(event) {
 });
 
 
+
 $('.nav_all').click(function(event) {
   $('.item').removeClass('hide');
   $('item').addClass('show');
 });
+
+
+
+
+$('.nav_solid').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.solid').removeClass('hide');
+  $('.item.solid').addClass('show');
+});
+
+
+$('.nav_velvet').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.velvet').removeClass('hide');
+  $('.item.velvet').addClass('show');
+});
+
+
+$('.nav_polished').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.polished').removeClass('hide');
+  $('.item.polished').addClass('show');
+});
+
+$('.nav_soft').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.soft').removeClass('hide');
+  $('.item.soft').addClass('show');
+});
+
+$('.nav_metallic').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.metallic').removeClass('hide');
+  $('.item.metallic').addClass('show');
+});
+
+
+$('.nav_fabric').click(function(event) {
+  $('.item').removeClass('show');
+  $('.item').addClass('hide');
+
+  $('.item.fabric').removeClass('hide');
+  $('.item.fabric').addClass('show');
+});
+
+
 
 $('.nav_thumb').click(function(event){
   $('.item').addClass('thumb');
@@ -42,8 +99,6 @@ $('.nav_full').click(function(event){
   $('.item').addClass('full');
   $('.item').removeClass('thumb');
 });
-
-
 
 
 
